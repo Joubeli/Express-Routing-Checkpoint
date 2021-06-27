86e86a48e16821636e725c3da1d2da4f19365a84
imene joubeli <joubeliimen@gmail.com>
2021-06-27 22:27:38 +0100

    update middlware

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
-let express = require("express"); 
-let app     = express();
-let path    = require("path");
-
-
-const logger=(req,res,next)=>{
-  let date = new Date()
-  let day = date.getDay()
-  let hour = date.getHours()
-  console.log(day) 
-  console.log(hour) 
-  if (hour < 9 || hour > 16 ||  day < 1  ||day > 5) {
-          res.send('Application is only available during working hours Monday to Friday,  from 9 to 17')
-  }
-          next();
-}
-
-app.use(logger)
+let express = require("express");
+let app = express();
+let path = require("path");
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/index',(req,res)=>{
+/* const Logger = (req,res,next) => 
+{
+        let date = new Date()
+        let day = date.getDay()
+        let hour = date.getHours()
+        
+        if (hour < 9 || hour > 16 || day > 5 || day < 1) {
+                res.render('error')
+        }
+                next();
+}
+app.use(Logger)  */
+
+/* app.use((request, response, next) => {
+  let today = new Date();
+  console.log(today)
+  if (today.getDay() === 0 || today.getDay() === 6) {
+    response.end('Application is only available during working hours Monday to Friday,  from 9 to 17');
+  } 
+  else {
+  next();
+  
+   }
+}); */
+
+
+app.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/contact',(req,res)=>{
-  res.sendFile(path.join(__dirname+'/contact.html'));
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname + '/contact.html'));
 });
 
-app.get('/services',(req,res)=>{
-  res.redirect(path.join(__dirname + '/services.html'));
+app.get('/services', (req, res) => {
+  res.sendFile(path.join(__dirname + '/services.html'));
 });
 
-app.listen(3000);
+app.listen(5000);
 
-console.log("Running at Port 3000");
+console.log("Running at Port 5000");
