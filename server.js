72fe03977efc4837a655f8768e90ee181cd3902d
imene joubeli <joubeliimen@gmail.com>
2021-06-28 13:26:33 +0100

    correct timer

--- server.js
+++ server.js
@@ -2,32 +2,22 @@ let express = require("express");
 let app = express();
 let path = require("path");
 
-app.use(express.static(__dirname + '/public'));
 
-/* const Logger = (req,res,next) => 
+
+const Logger = (req,res,next) => 
 {
         let date = new Date()
         let day = date.getDay()
         let hour = date.getHours()
         
         if (hour < 9 || hour > 16 || day > 5 || day < 1) {
-                res.render('error')
+                res.send('The web application is only available during working hours (Monday to Friday,  from 9 to 17)')
         }
                 next();
 }
-app.use(Logger)  */
+app.use(Logger) 
+app.use(express.static(__dirname + '/public'));
 
-/* app.use((request, response, next) => {
-  let today = new Date();
-  console.log(today)
-  if (today.getDay() === 0 || today.getDay() === 6) {
-    response.end('Application is only available during working hours Monday to Friday,  from 9 to 17');
-  } 
-  else {
-  next();
-  
-   }
-}); */
 
 
 app.get('/index', (req, res) => {
